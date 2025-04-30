d1ffc1316ccded15ef6d056e1ee49961df61f82e
Azeez <azeez.oyelowo11@gmail.com>
2025-04-30 15:34:34 -0500

    Yes or no section

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
 echo "Welcome to this plant care simulator"
 echo "What is your name?"
 read username 
 echo "Hello $username"
 sleep 3
-exit
+echo "Do you want to plant a new seed? (y/n)"
+read answer
+
+if [[ "$answer == "y" || "$answer" == "Y" ]]; then
+	echo "You have dug a whole and planted a small seed"
+	echo -n "Medium size"
+	read size
+	echo "A $size seed is now planted. Good luck growing it!""
+else 
+	echo "Exiting the game..."
+	sleep 3
+	echo "Godbye."
+	exit 0
+fi
