453b80fb67229d65b48ddc38090acfc2e7fcef3f
Azeez <azeez.oyelowo11@gmail.com>
2025-05-05 15:12:23 -0500

    From in this digital world

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -21,10 +21,16 @@ else
 	echo "Goodbye."
 	exit 0
 fi
+sleep 2
 echo "In this digital world, time moves much faster than it does outside reality, and that mere minutes or seconds of your time can be hours, days, or months in this place"
+sleep 2
 echo "Do you want to wait for your seed to grow? (y/n)"
-if [[ $answer == "y" || $answer == "Y" ]]; then
-	wait 1 day
+read grow
+
+if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
+	echo "Waiting one day for seed to grow..."
+	sleep 2
+	echo "The seed will take 3 days to grow"
 else 
 	echo "Exiting the game..."
 	sleep 2
