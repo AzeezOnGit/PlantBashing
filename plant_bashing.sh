736f7adc3438e8a77a7675e3d98d71c745f64a01
Azeez <azeez.oyelowo11@gmail.com>
2025-05-08 15:07:54 -0500

    From in this world

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -51,14 +51,40 @@ echo "Do you want to grow it for 1 more day or leave now? (y/n)"
 read grow
 
 if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
-	echo "Day 4: Wating for Day 4..."
+	echo "Wating for Day 4..."
 	sleep 2
 	echo "Nothing happened"
 	
-
 else
 	echo "Goodbye. Thanks for playing!"
 	sleep 2
 	exit 0
 fi
 	
+echo "Do you want to grow it for 1 more day or leave now? (y/n)"
+read grow
+
+if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
+	echo "Wating for Day 5..."
+	sleep 2
+	echo "Nothing happened in during the day, but something could happen later on."
+
+else
+	echo "Goodbye. Thanks for playing!"
+	sleep 2
+	exit 0
+fi
+
+echo "Do you want to grow it for 1 more day or leave now? (y/n)"
+read grow
+ 
+ if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
+	echo "Wating for Day 6..."
+	sleep 2
+	echo "It's morning time and the seed grew overnight into a sapling! It is now 2cm in height and has 2 leaves."	
+
+else
+	echo "Goodbye. Thanks for playing!"
+	sleep 2
+	exit 0
+fi
