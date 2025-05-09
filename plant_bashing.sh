a146e01ff9f42116a46c5ec1ccb6fbe70da1a10a
Azeez <azeez.oyelowo11@gmail.com>
2025-05-09 14:51:57 -0500

    FRom in

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Welcome to this plant care simulator"
+echo "Welcome to this plant care simulator!"
 echo "What is your name?"
 read username 
 echo "Hello $username"
@@ -11,26 +11,26 @@ read answer
 if [[ $answer == "y" || $answer == "Y" ]]; then
 	echo "You have dug a hole and planted a seed..."
 	sleep 2
-	echo "It is the size of a peanut"
+	echo "It is the size of a peanut."
 	sleep 2
 	echo "The seed has now been planted. Good luck growing it!"
 	
 else 
 	echo "Exiting the game..."
-	sleep 2
+	sleep 1
 	echo "Goodbye."
 	exit 0
 fi
-sleep 2
-echo "In this digital world, time moves much faster than it does outside reality. Mere minutes or seconds of your time can be hours, days, or months in this place"
-sleep 2
+sleep 1
+echo "In this digital world, time moves much faster than it does outside reality. Mere minutes or seconds of your time can be hours, days, or even months in this place."
+sleep 1
 echo "Do you want to wait 1 day for your seed to grow? (y/n)"
 read grow
 
 if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
 	echo "Waiting one day for seed to grow..."
 	sleep 2
-	echo "The seed was planted"
+	echo "The seed has been planted."
 
 else 
 	echo "Exiting the game..."
@@ -90,7 +90,7 @@ read grow
 if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
 	echo "Wating for Day 5..."
 	sleep 2
-	echo "Nothing happened in during the day, but something could happen later on."
+	echo "Nothing happened during the day, but something could happen later on."
 
 else
 	echo "Goodbye. Thanks for playing!"
@@ -118,7 +118,7 @@ read grow
 if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
 	echo "Wating for Day 7..."
 	sleep 2
-	echo "Wow! The seed grew overnight again and it is now 4cm in height and has 4 leaves "
+	echo "Wow! The seed grew overnight again and it is now 4cm in height and has 4 leaves."
 
 else
 	echo "Goodbye. Thanks for playing!"
@@ -132,7 +132,49 @@ read grow
 if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
 	echo "Wating for Day 8..."
 	sleep 2
-	echo "Hooray! The seed grew overnight again and it is now 6cm in height and has 6 leaves "
+	echo "Hooray! The seed grew overnight again and it is now 6cm in height and has 6 leaves."
+
+else
+	echo "Goodbye. Thanks for playing!"
+	sleep 2
+	exit 0
+fi
+
+echo "Do you want to grow it for 1 more day or leave now? (y/n)"
+read grow
+
+if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
+	echo "Wating for Day 9..."
+	sleep 2
+	echo "Yipee! The seed grew overnight again and it is now 8cm in height and has 8 leaves."
+
+else
+	echo "Goodbye. Thanks for playing!"
+	sleep 2
+	exit 0
+fi
+
+echo "Do you want to grow it for 1 more day or leave now? (y/n)"
+read grow
+
+if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
+	echo "Wating for Day 10..."
+	sleep 1
+	echo "OMG! The seed grew overnight again and it is now 10cm in height and has 10 leaves."
+
+else
+	echo "Goodbye. Thanks for playing!"
+	sleep 1
+	exit 0
+fi
+
+echo "Do you want to grow it for 1 more day or leave now? (y/n)"
+read grow
+
+if [[ "$grow" == "y" || "$grow" == "Y" ]]; then
+	echo "Wating for Day 11..."
+	sleep 2
+	echo "Wowzers! The seed grew overnight again and it is now 12cm in height and has 12 leaves. It sure is getting lengthy."
 
 else
 	echo "Goodbye. Thanks for playing!"
