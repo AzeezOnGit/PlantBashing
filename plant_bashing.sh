3b9df9cbf1d1d28fdfd92de57063a1315b1bc9a2
Azeez <azeez.oyelowo11@gmail.com>
2025-05-26 15:18:06 -0500

    New script

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,84 +1,150 @@
 #!/bin/bash
 
-# Initialize variables
-playing=true
-first_play=true
-plant_name="Morpheus"
-player_name=""
+# Plant Bashing Game - Bash Script Simulator
 
-while $playing; do
-    if $first_play; then
-        echo "Welcome to the Plant Care Simulator!"
-        echo "What is your name?"
-        read player_name
-        echo "Hello $player_name!"
-        first_play=false
+# Variables
+playGame=true
+firstTime=true
+userName=""
+plantName="Morpheus"
+
+# Function to pause and simulate time
+pause() {
+    echo ""
+    echo "..."
+    sleep 2
+    echo ""
+}
+
+# Start the game loop
+while [ "$playGame" == true ]; do
+
+    # Collect user's name only on the first play
+    if [ "$firstTime" == true ]; then
+        echo "🌿 Welcome to the Plant Bashing Garden! 🌿"
+        read -p "Before we begin, what's your name? " userName
+        echo "Nice to meet you, $userName! Let's grow something amazing together!"
+        firstTime=false
     else
-        echo "Welcome back, $player_name!"
-        echo "Do you want to change your plant's name? (y/n)"
-        read change_name
-        if [[ $change_name == "y" || $change_name == "Y" ]]; then
-            echo "What do you want to name your plant?"
-            read plant_name
-        else
-            echo "Keeping the plant name as '$plant_name'."
-        fi
+        echo ""
+        echo "🌿 Welcome back to the Plant Bashing Garden, $userName! 🌿"
     fi
 
-    echo "Do you want to plant a new seed? (y/n)"
-    read answer
-    if [[ $answer != "y" && $answer != "Y" ]]; then
-        echo "Goodbye, $player_name. Thanks for playing!"
-        playing=false
-        break
+    # Check if user wants to change plant name (not on first play)
+    if [ "$firstTime" == false ]; then
+        read -p "Would you like to change your plant's name? (Yes/No) " changePlant
+        case $changePlant in
+            [Yy]* )
+                read -p "What would you like to name your plant? " plantName
+                echo "Your plant will now be called $plantName!"
+                ;;
+            * )
+                echo "We'll stick with $plantName then!"
+                ;;
+        esac
     fi
 
-    echo "You have dug a hole and planted a seed named '$plant_name'..."
-    sleep 1
-    echo "It is the size of a peanut."
-    sleep 1
+    # Ask if user wants to plant a new seed
+    read -p "Would you like to plant a new seed? (Yes/No) " plantChoice
+    case $plantChoice in
+        [Yy]* )
+            echo "You dig a hole and plant a small seed the size of a pebble."
+            ;;
+        * )
+            echo "Alright, maybe next time. Goodbye, $userName!"
+            playGame=false
+            break
+            ;;
+    esac
 
-    day=1
-    height=2
-    leaves=2
-    germinated=false
+    # Time simulation intro
+    echo "In the digital world of this computer, time moves much faster than in your reality."
+    echo "Minutes or seconds for you can be days, weeks, or months here!"
+    pause
 
-    while true; do
-        echo "Do you want to wait for Day $day and see if '$plant_name' grows? (y/n)"
-        read grow
-        if [[ $grow != "y" && $grow != "Y" ]]; then
-            echo "Goodbye, $player_name. Thanks for playing!"
-            playing=false
-            break 2
-        fi
+    # Plant growth stages
+    totalDays=1
+    height=0
+    leaves=0
 
-        echo "Waiting for Day $day..."
-        sleep 1
+    # Initial plant naming for first playthrough
+    if [ "$firstTime" == true ]; then
+        read -p "Would you like to name your plant? (Yes/No) " nameChoice
+        case $nameChoice in
+            [Yy]* )
+                read -p "What would you like to name your plant? " plantName
+                echo "Your plant will be called $plantName!"
+                ;;
+            * )
+                echo "We'll go with $plantName then!"
+                ;;
+        esac
+    fi
 
-        if [[ $day -eq 3 && $germinated == false ]]; then
-            echo "Wow! '$plant_name' germinated overnight!"
-            germinated=true
-        elif [[ $day -eq 21 ]]; then
-            echo "Oh no! '$plant_name' grew overnight to 34cm with 34 leaves, but it has now reached the end of its lifecycle."
-            echo "Thanks for playing!"
-            sleep 1
-            echo "Would you like to play again? (y/n)"
-            read play_again
-            if [[ $play_again != "y" && $play_again != "Y" ]]; then
-                echo "Goodbye, $player_name. Thanks for playing!"
-                playing=false
-                break 2
-            else
-                echo "Restarting the game..."
-                sleep 1
+    echo "Day 1 - You planted $plantName."
+    pause
+
+    # Day 2
+    echo "Day 2 - Nothing happened."
+    ((totalDays++))
+    pause
+
+    # Day 3 - Germination
+    echo "Day 3 - $plantName germinated overnight!"
+    ((totalDays++))
+    pause
+
+    # Ask user to wait or leave
+    while [ $totalDays -lt 21 ]; do
+        read -p "Would you like to wait another day to see $plantName grow, or leave? (Wait/Leave) " choice
+        case $choice in
+            [Ww]* )
+                ((totalDays++))
+                # Growth logic: After Day 6, plant grows 2cm and 2 leaves each day
+                if [ $totalDays -eq 6 ]; then
+                    echo "Day 6 - $plantName grew overnight into a sapling!"
+                elif [ $totalDays -gt 6 ]; then
+                    height=$(( (totalDays - 6) * 2 ))
+                    leaves=$(( (totalDays - 6) * 2 ))
+                    echo "Day $totalDays - $plantName is now $height cm tall and has $leaves leaves!"
+                else
+                    echo "Day $totalDays - Nothing happened."
+                fi
+                ;;
+            [Ll]* )
+                echo "Your sapling, $plantName, is lost to time. Goodbye, $userName!"
+                pause
                 break
-            fi
-        else
-            echo "'$plant_name' grew overnight! It is now ${height}cm tall and has ${leaves} leaves."
-            height=$((height + 2))
-            leaves=$((leaves + 2))
-        fi
+                ;;
+            * )
+                echo "Please answer with Wait or Leave."
+                ;;
+        esac
 
-        day=$((day + 1))
+        # Check if totalDays reached 21
+        if [ $totalDays -ge 21 ]; then
+            echo ""
+            echo "🌿 $plantName has fully matured! 🌿"
+            echo "Total Age: 21 days"
+            echo "Final Height: 34cm"
+            echo "Leaf Total: 34"
+            pause
+            break
+        fi
     done
+
+    # Ask if user wants to play again
+    read -p "Would you like to play again and grow a new plant? (Yes/No) " playAgain
+    case $playAgain in
+        [Yy]* )
+            echo "Alright! Let's start over!"
+            ;;
+        * )
+            echo "Thanks for playing, $userName! See you next time!"
+            playGame=false
+            ;;
+    esac
+
 done
+
+echo "Goodbye, $userName! 🌸"
