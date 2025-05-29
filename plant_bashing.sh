eb38c4d0fdea8c90928bf53878067db7e6752c51
root <root@vbox.myguest.virtualbox.org>
2025-05-28 23:20:34 -0500

    recent changes

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -116,12 +116,12 @@ while [ "$keep_playing" == true ]; do
 
         if [ $plant_age -eq 21 ]; then
             echo "The life cycle has come to an end."
-            break
+            game_over=true
         fi
 
         ask_to_wait
         if [ $? -ne 0 ]; then
-            break
+            game_over=true
         fi
     done
 
